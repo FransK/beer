82469d421968342d24917f5eee9a0822b7790034
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2017-02-19 10:34:24 -0800

    Update index.html and routing for local development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/beer', pathMatch: 'full' },
-  { path: 'beer', loadChildren: 'app/beer/beer.module#BeerModule' }
+  { path: '', loadChildren: 'app/beer/beer.module#BeerModule' },
 ]
 
 @NgModule({
