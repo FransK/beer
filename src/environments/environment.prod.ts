f1c934ebd0107c7eee501197c219d4e585656a8e
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2017-06-05 13:50:48 -0700

    Setting up firebase deploy and initial deployment of website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCaxKxkhRt9NDlR4fBMrfbg-VPPBheh8ws',
+    authDomain: 'brew-review.firebaseapp.com',
+    databaseURL: 'https://brew-review.firebaseio.com',
+    projectId: 'brew-review',
+    storageBucket: 'brew-review.appspot.com',
+    messagingSenderId: '233919008511'
+  }
 };
